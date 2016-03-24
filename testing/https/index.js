79c91f6ec1e698dcdec362bead919501ddc0f934
AlexLeoTW <alex@alexleo.click>
2016-03-25 02:49:20 +0800

    update https testing file

--- testing/https/index.js
+++ testing/https/index.js
@@ -1,14 +1,26 @@
 var https = require('https');
 var fs = require('fs');
+var debug = require('debug')('Dev:server');
 
 var options= {
   key: fs.readFileSync('key.pem'),
   cert: fs.readFileSync('cert.pem')
 };
 
-https.createServer(options, function(req, res) {
+var httpsServer = https.createServer(options, function(req, res) {
   res.writeHead(200);
   res.end('hello world\n');
-}).listen(8000);
+});
 
-console.log('lostining port 8000');
+httpsServer.listen(8000);
+
+httpsServer.on('listening', onListening);
+
+function onListening() {
+  var addr = httpsServer.address();
+  var bind = typeof addr === 'string' ?
+    'pipe ' + addr : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+console.log('Reach the end of file!!!');
